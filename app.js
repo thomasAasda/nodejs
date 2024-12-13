7ccdc1db85241cc136ba7f4b30c54f2e00912587
thomasAasda <92527406+thomasAasda@users.noreply.github.com>
2024-12-13 19:39:08 +0100

    app.js

--- app.js
+++ app.js
@@ -1,35 +1,27 @@
 const express = require('express');
-const cors = require('cors'); // Importa CORS
-const path = require('path');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 8080;
 
 let scripts = {};
 
-// Abilita CORS per tutte le origini
-app.use(cors());
-
-// Se vuoi limitare l'accesso solo a determinati domini, puoi farlo così:
-// app.use(cors({
-//     origin: 'https://1480224298-atari-embeds.googleusercontent.com' // Specifica il dominio che deve avere accesso
-// }));
-
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());  // Abilita CORS
 app.use(express.json());
 
+// Gestisce la richiesta GET per ottenere uno script Lua
 app.get('/key/:id', (req, res) => {
     const gameId = req.params.id;
     const script = scripts[gameId] || '';
     res.send(script);
 });
 
+// Gestisce la richiesta POST per inviare uno script Lua
 app.post('/key/:id', (req, res) => {
     const gameId = req.params.id;
     const script = req.body.script;
 
-    if (!gameId) {
-        res.status(400).send('No Game ID Specified');
-        return;
+    if (!gameId || !script) {
+        return res.status(400).send('Game ID or script missing');
     }
 
     scripts[gameId] = script;
